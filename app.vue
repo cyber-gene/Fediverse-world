78a5fd33be3381a7a6265faf91af7b3f2e042af6
cybergene <cybergene@protonmail.com>
2025-02-25 11:14:58 +0900

    初期設定と基本的なコンポーネントの追加

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <Background />
+   </div>
+</template>
+<script lang="ts" setup>
+import Background from './components/Background.vue';
+</script>  
---
+++ components/Background.vue
@@ -0,0 +1,84 @@
+<template>
+   <canvas ref="canvas" class="fixed top-0 left-0 w-full h-full"></canvas>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+const canvas = ref<HTMLCanvasElement | null>(null)
+const ctx = ref<CanvasRenderingContext2D | null>(null)
+
+const numStars = 500
+const stars = ref<{ x: number; y: number; z: number }[]>([])
+
+const initCanvas = () => {
+  if (!canvas.value) return
+  canvas.value.width = window.innerWidth
+  canvas.value.height = window.innerHeight
+  ctx.value = canvas.value.getContext('2d') || null
+}
+
+const createStars = () => {
+  stars.value = Array.from({ length: numStars }, () => ({
+    x: Math.random() * (canvas.value?.width || 0),
+    y: Math.random() * (canvas.value?.height || 0),
+    z: Math.random() * (canvas.value?.width || 0)
+  }))
+}
+
+const updateStars = () => {
+  stars.value.forEach((star) => {
+    star.z -= 2
+    if (star.z <= 0) {
+      star.z = canvas.value?.width || 0
+      star.x = Math.random() * (canvas.value?.width || 0)
+      star.y = Math.random() * (canvas.value?.height || 0)
+    }
+  })
+}
+
+const drawStars = () => {
+  if (!ctx.value || !canvas.value) return
+
+  ctx.value.fillStyle = 'black'
+  ctx.value.fillRect(0, 0, canvas.value.width, canvas.value.height)
+
+  stars.value.forEach((star) => {
+    let factor = 128.0 / star.z
+    let x = (star.x - (canvas.value?.width || 0) / 2) * factor + (canvas.value?.width || 0) / 2
+    let y = (star.y - (canvas.value?.height || 0) / 2) * factor + (canvas.value?.height || 0) / 2
+    let radius = factor
+
+    if (ctx.value) {
+      ctx.value.beginPath()
+      ctx.value.arc(x, y, radius, 0, Math.PI * 2)
+      ctx.value.fillStyle = 'white'
+      ctx.value.fill()
+    }
+  })
+}
+
+const animate = () => {
+  updateStars()
+  drawStars()
+  requestAnimationFrame(animate)
+}
+
+onMounted(() => {
+  initCanvas()
+  createStars()
+  animate()
+  window.addEventListener('resize', initCanvas)
+})
+</script>
+
+<style>
+canvas {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  background-color: black;
+}
+</style>
